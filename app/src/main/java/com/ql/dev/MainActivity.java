46a2f78bfdd2f2b6f606ad11de8b8501c9ab38f6
ShaKristen13kij <wei52610@yeah.net>
2021-06-30 16:34:34 +0800

    升级Androidx版本

--- app/src/main/java/com/ql/dev/MainActivity.java
+++ app/src/main/java/com/ql/dev/MainActivity.java
@@ -1,9 +1,10 @@
 package com.ql.dev;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.ql.dev.library.SwitchView;
 import com.ql.dev.library.TitleNavView;
--- library/src/main/java/com/ql/dev/library/TitleNavView.java
+++ library/src/main/java/com/ql/dev/library/TitleNavView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -12,6 +11,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 import static com.ql.dev.library.StringUtils.getHttpBitmap;
 
